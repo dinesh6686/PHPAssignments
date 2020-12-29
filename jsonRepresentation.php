b30dbbacaa4a0c9c84b1c4cf778dd22cedc2239d
Dinesh Venkat <dinesh6686@gmail.com>
2020-12-29 16:33:05 +0530

    changes to jsonRepresentation

--- jsonRepresentation.php
+++ jsonRepresentation.php
@@ -1,9 +1,4 @@
 <?php
-$person = array(
-    "Name" => "John Wick",
-    "age" => "30",
-    "city" => "New York"
-);
-$json = json_encode($person, JSON_PRETTY_PRINT);
-echo $json;
+$person = array("Name" => "John Wick", "age" => "30", "city" => "New York");
+echo json_encode($person);
 ?>
