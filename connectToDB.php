75f9c7ef7d6d429c4e93cc02d357201ce39dfd8f
Dinesh Venkat <dinesh6686@gmail.com>
2020-12-30 20:44:18 +0530

    assignment to make aconnection to MySQL database and add db and tables if not exists

---
+++ connectToDB.php
@@ -0,0 +1,33 @@
+<?php
+    function makeConnection($servername, $username, $password, $db){
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+        // Check connection
+        if ($conn->connect_error) echo "Connection failed: " . $conn->connect_error;
+        else echo "Connection established Successfully!<br>";
+
+        //Create database if not found
+        $sql = "CREATE DATABASE IF NOT EXISTS magento241";
+        if($conn->query($sql) === TRUE) echo "Database created successfully!<br>";
+        else echo "Error creating database: ".$conn->error;
+
+        //Use db if already exists
+        $usedb = $conn->query("USE $db");
+
+        
+        //Create a table 6470exerciseusers if doesnt exists
+        $table = "CREATE TABLE IF NOT EXISTS 6470exerciseusers (
+            USERNAME VARCHAR(100) UNIQUE,
+            PASSWORD_HASH CHAR(40),
+            PHONE VARCHAR(10) NOT NULL
+        )";
+        
+        $result = $conn->query($table);
+        if ($result === TRUE) {
+            echo "Table got created successfully!<br>";
+        }
+        else echo "Error creating the table: ".$conn->error;
+        $conn->close();
+    }
+    makeConnection("localhost","root","","magento241")
+?>
